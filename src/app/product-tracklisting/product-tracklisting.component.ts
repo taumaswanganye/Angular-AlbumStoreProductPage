5c39af132f8629f2a4488231b4bd1fb45442d317
taumaswanganye <tau.maswanganye@gmail.com>
2021-11-12 08:28:23 +0200

    Solution

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Album } from '../album';
+
 import { ProductService } from '../product.service';
 
 @Component({
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
@@ -18,10 +19,10 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
